51adcdf69ad8417d28f54b9a884ecd1ce773bbe9
Kevin <kjh311@gmail.com>
2019-07-11 09:23:19 -0700

    first

---
+++ script.js
@@ -0,0 +1,17 @@
+const Http = new XMLHttpRequest();
+const url = 'https://jsonplaceholder.typicode.com/posts';
+Http.open("GET", url);
+Http.send();
+
+
+Http.onreadystatechange=(e)=>{
+	const response = JSON.parse(Http.responseText);
+
+for(var i = 0; i < response.length - 1; i++){
+    var li = document.createElement('li');
+    li.innerHTML = response[i].title;  
+    document.getElementById('list-ul').appendChild(li);
+	}
+
+	console.log(response);
+}
